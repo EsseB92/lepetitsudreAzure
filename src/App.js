58b5b6ddf590b5e71df9dd8ec67b29247ab082fc
EsseB92 <62557771+souljaboy92@users.noreply.github.com>
2021-05-20 03:04:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { FilePond } from 'react-filepond';
-import 'filepond/dist/filepond.min.css';
 
 function App() {
   return (
     <div className="App">
-    	<input type="file">
+    	<input type="file"/>
     </div>
   );
 }
