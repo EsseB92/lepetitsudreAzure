f6f3289dabec9cdce9e42d5745e0a24b2a46d9b6
EsseB92 <62557771+souljaboy92@users.noreply.github.com>
2021-05-20 03:11:13 +0200

    New

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
 
 function App() {
   return (
     <div className="App">
-    	<input type="file"/>
+  		<FilePond server="https://cnamtpfuncayw.azurewebsites.net/api/UploadPhoto?code=3EuwYbV4M0VmzwQXY4wSkbchkWtmRsd0bhX/7PHfoySM2D2RZwpmDQ=="/>
     </div>
   );
 }
