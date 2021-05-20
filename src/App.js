87a0eb41469231351a83cdd0616b9f5390256c8c
EsseB92 <62557771+souljaboy92@users.noreply.github.com>
2021-05-20 02:49:19 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 
 function App() {
+
+  const context = useContext(UploadsViewStateContext);
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  const uploadFiles = (files: FileList | null) =>
+    files && context.uploadItems(files);
+
+  const showFileDialog = () =>
+    inputFileRef.current && inputFileRef.current.click()
+
   return (
-  	<input type="file" id="file-selector" multiple>
-	<script>
-	  const fileSelector = document.getElementById('file-selector');
-	  fileSelector.addEventListener('change', (event) => {
-	    const fileList = event.target.files;
-	    console.log(fileList);
-	  });
-	</script>
+	<div className="input-file">
+      <input 
+        style={{ display: 'none' }}
+        ref={inputFileRef}
+        type="file"
+        multiple={true}
+        onChange={e => uploadFiles(e.target.files)}
+      />
+      <button onClick={() => showFileDialog()}>
+        Click here to Upload File
+      </button>
+    </div>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
