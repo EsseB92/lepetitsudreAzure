f39ac6ae64a2c15d854180e9c75b82bb1a62636e
EsseB92 <62557771+souljaboy92@users.noreply.github.com>
2021-05-20 09:57:17 +0200

    Test app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import logo from './logo.svg';
+import './App.css';
 import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 
 function App() {
   return (
     <div className="App">
+    	<header className="App-header">
+	        <img src={logo} className="App-logo" alt="logo" />
+	        <p>
+	          Edit <code>src/App.js</code> and save to reload.
+	        </p>
+	        <a
+	          className="App-link"
+	          href="https://reactjs.org"
+	          target="_blank"
+	          rel="noopener noreferrer"
+	        >
+	          Learn React by Aymeric
+	        </a>
+      	</header>
+
   		<FilePond server="https://lepetitsudredescimage.azurewebsites.net/api/AnalyseImage?code=mpjotMyJJ9J1NgzZtGuMPKYo5fWhMVkn9aHR8U8DMgcRQce0KcJO5A=="/>
     </div>
   );
